ed82b599bcab3eded866103bcf61ab19c1a62090
Dariya Babu Malapati <117331227+dariyababumalapati@users.noreply.github.com>
2023-12-26 18:34:47 +0100

    starting kgm novel

---
+++ kgm_url_extraction.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+
+from database_actions_module.db_initiator import create_connection
+
+data_base = 'novel_web_scraping'
+connection = create_connection(data_base)
+cursor = connection.cursor()
+
+with open('kgm.html', 'r', encoding='utf-8') as f:
+    html_content = f.read()
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+specific_ul = soup.find('ul', id='chapterList')
+
+# print(specific_ul)
+
+chapters = []
+
+for li in specific_ul.find_all('li')[::-1]:
+
+    chapter_title = li.a.get_text(strip=True)
+    
+    url_part = li.a.get('href')
+    url = f"https://www.uukanshu.net/{url_part}"
+
+    chapters.append((chapter_title, url))
+
+for chapter in chapters:
+
+    # print(chapter_title)
+    cursor.execute("""
+        INSERT INTO king_of_german_mercenaries (chapter_title, url) VALUES (%s, %s)       
+    """, chapter
+    )
+
+connection.commit()
+cursor.close()
+connection.close()
--- scrap/url_extraction.py
+++ scrap/url_extraction.py
@@ -1,18 +1,20 @@
+import os
 from bs4 import BeautifulSoup
 import mysql.connector
 
-
+with open('table_index.html', 'r', encoding='utf-8') as f:
+    html_content = f.read()
 
 # Parse the HTML content
 soup = BeautifulSoup(html_content, 'html.parser')
 
 # Establish a connection to your MySQL database
 connection = mysql.connector.connect(
-    host='your_host',
-    user='your_username',
-    password='your_password',
-    database='your_database'
-)
+    host='localhost',
+    user='root',
+    password=os.environ.get('MYSQL_PASSWORD'),
+    database='novel_web_scraping'
+    )
 
 # Create a cursor to execute SQL queries
 cursor = connection.cursor()
@@ -26,7 +28,14 @@ cursor.execute('''
 ''')
 
 # Extract URLs and chapter numbers and insert them into the table
-for li in soup.find_all('li'):
+specific_ul = soup.find('div', class_='catalog', id='catalog').find('ul')
+
+# print(specific_ul)
+
+with open('url_list.html', 'w', encoding='utf-8') as f:
+    f.write(specific_ul.prettify())
+
+for li in specific_ul.find_all('li'):
     chapter_number = li.get('data-num')
     url = li.a.get('href')
     
@@ -37,3 +46,4 @@ for li in soup.find_all('li'):
 # Commit changes and close the connection
 connection.commit()
 connection.close()
+    
